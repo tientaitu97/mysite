6f9c76d1c154a99edc845c59977d1d09511e21e0
tiendd@fabbi.io <tiendd@fabbi.io>
2020-04-20 20:14:02 +0700

    upload n1

---
+++ blog/management/commands/postfaker.py
@@ -0,0 +1,29 @@
+import random
+from faker import Faker
+from blog.models import Post
+from django.contrib.auth.models import User
+from django.utils import timezone
+from django.core.management.base import BaseCommand
+
+faker = Faker()
+
+
+class Command(BaseCommand):
+    help = 'Closes the specified post for voting'
+
+    def add_arguments(self, parser):
+        parser.add_argument('count', type=int)
+
+    def handle(self, *args, **options):
+        count = options['count']
+        list_user = User.objects.all()
+        for _ in range(0, count):
+            Post.objects.create(title=faker.name(),
+                                author=random.choice(list_user),
+
+                                slug=faker.name(),
+                                body=faker.text(),
+                                status=random.choice(['published', 'draft']),
+                                created=timezone.now(),
+                                updated=timezone.now(),
+                                )
---
+++ blog/models.py
@@ -0,0 +1,20 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+
+# Create your models here.
+class Post(models.Model):
+    STATUS_CHOICES = (
+        ('draft', 'Draft'),
+        ('published', 'Published')
+    )
+    title = models.CharField(max_length=100, null=True, blank=True, unique=False)
+    slug = models.SlugField(max_length=120)
+    author = models.ForeignKey(User, related_name='blog_posts', on_delete=models.DO_NOTHING)
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
+
+    def __str__(self):
+        return self.title
